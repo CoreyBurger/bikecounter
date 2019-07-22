3683a8e6af4416b04ce87dbb4e0384afe9cb5529
CoreyBurger <corey.burger@gmail.com>
2019-07-22 09:20:47 -0700

    edits to visualizecounter and start html framework

--- visualizeCounter.py
+++ visualizeCounter.py
@@ -3,15 +3,38 @@ import json
 import altair
 import pandas
 import datetime
+import bs4
+import os
 
-data = pandas.read_csv=("counts-100117730.csv")
+#define constants
+workingDir = os.getcwd()
 
-heatmap = altair.Chart(data).mark_rect().encode(
-    altair.X('hours(Date):O'),
-    altair.Y('Date:O', timeUnit='year'),
+#load data
+countFile = "counts-100117730.csv"
+data = pandas.read_csv(countFile,parse_dates=['Date'])
+
+#
+dailyCount = data.resample('D',on='Date').sum()
+dailyCount['Day'] = dailyCount.index
+dailyCount = dailyCount.loc[dailyCount['Day'] >= '2015-01-01']
+
+heatmap = altair.Chart(dailyCount).mark_rect().encode(
+    altair.X('day(Day):O'),
+    altair.Y('Day:O', timeUnit='year'),
     altair.Color('mean(Count):Q')
 )
 
 chart = heatmap
-   
-chart.save('bikecounter/altairTest.json')
+
+#open the HTML doc   
+with open (workingDir + '\\counterVisual.html') as counterPage:
+    page = bs4.BeautifulSoup(counterPage)
+
+#write out the first data
+testV1spec = "var testV1Spec =" + chart.to_json() + "; vegaEmbed('#vis1', testV1Spec); "
+page.find(id='vis1script').string.replace_with(testV1spec)
+
+#write out the HTML
+page = bs4.BeautifulSoup.prettify(page)
+with open (workingDir + '\\counterVisual.html',"w") as counterPage:
+    counterPage.write(page)

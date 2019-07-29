283928e0e224543fbd0b5257232cc3255f0d4992
CoreyBurger <corey.burger@gmail.com>
2019-07-29 10:35:23 -0700

    added (broken) yearly visualization

--- visualizeCounter.py
+++ visualizeCounter.py
@@ -9,7 +9,7 @@ import csv
 
 #define constants
 workingDir = os.getcwd()
-yesterday = datetime.date.today() - datetime.timedelta(2)
+yesterday = datetime.date.today() - datetime.timedelta(1)
 yesterday = yesterday.strftime('%Y-%m-%d')
 
 #load data
@@ -36,14 +36,16 @@ ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
 print(ordinal(dailyRankThisYear))
 
 #craft the string for yeserday
-countString ="Yesterday saw " + str(yesterdayCount) + " bike rides, the " + str(ordinal(dailyRankThisYear)) + " busiest day of " + (datetime.date.today() - datetime.timedelta(2)).strftime('%Y') +  " and " + str(ordinal(dailyRankAll)) + " busiest overall."
+countString ="Yesterday saw " + str(yesterdayCount) + " bike rides, the " + str(ordinal(dailyRankThisYear)) + " busiest day of " + (datetime.date.today() - datetime.timedelta(1)).strftime('%Y') +  " and " + str(ordinal(dailyRankAll)) + " busiest overall."
 
 #add monthly & yearly cumulative total
+YearlyCumSum = dailyCount.groupby(dailyCount.index.to_period('y')).cumsum()
+YearlyCumSum.rename(columns={'Count':'YearlyCumSum'}, inplace=True)
 dailyCount['MonthlyCumSum'] = dailyCount.groupby(dailyCount.index.to_period('m')).cumsum()
-#dailyCount['YearlyCumSum'] = dailyCount.groupby(dailyCount.index.to_period('y')).cumsum()
+dailyCount = pandas.merge(dailyCount,YearlyCumSum,on='Date')
 
 #write that list out to a csv file
-#dailyCount.to_csv(countExportFile)
+dailyCount.to_csv(countExportFile)
 
 #resample to monthly count
 monthlyCount = data.resample('M',on='Date').sum()
@@ -58,28 +60,35 @@ heatmap = altair.Chart(dailyCount).mark_rect().encode(
     altair.X('day(Day):O'),
     altair.Y('Day:O', timeUnit='year'),
     altair.Color('mean(Count):Q')
-).properties(width=300,height=300)
+).properties(width=200,height=300)
 
 #Create monthly cumulative line chart
 monthlyLine = altair.Chart(currentMonth).mark_line().encode(
     altair.X('date(Day):O'),
     altair.Y('MonthlyCumSum', axis=altair.Axis(title='Monthly Cumulative Sum')),
     altair.Color('Day:O', timeUnit='year')  
-).properties(width=400,height=300)
+).properties(width=200,height=300)
 
 monthlyLineAvg = altair.Chart(currentMonth).mark_line(color='grey').encode(
     altair.X('date(Day):O'),
     altair.Y('mean(MonthlyCumSum)'),
-).properties(width=400,height=300)
+).properties(width=200,height=300)
 
 monthlyLineBand = altair.Chart(currentMonth).mark_errorband(extent='stdev',color='grey').encode(
     altair.X('date(Day):O'),
     altair.Y('MonthlyCumSum'),
-).properties(width=400,height=300)
+).properties(width=200,height=300)
 
-#chart = altair.hconcat(heatmap,monthlyLine,monthlyLineAvg)
 MonthlyChart = monthlyLineBand + monthlyLine + monthlyLineAvg
-chart = altair.hconcat(heatmap,MonthlyChart)
+
+#Create yearlycumulative line chart
+yearlyLine = altair.Chart(dailyCount).mark_line().encode(
+    altair.X('Day:O', timeUnit='month'),
+    altair.Y('YearlyCumSum', axis=altair.Axis(title='Yearly Cumulative Sum')),
+    altair.Color('Day:O', timeUnit='year')  
+).properties(width=200,height=300)
+
+chart = altair.hconcat(heatmap,MonthlyChart,yearlyLine)
 
 #open the HTML doc
 with open (workingDir + '\\counterVisual.html') as counterPage:
@@ -89,7 +98,7 @@ with open (workingDir + '\\counterVisual.html') as counterPage:
 page.find(id='counterMap').find('script').string.replace_with(mapHTML)
 
 #write out the yesterday string
-page.find(id="counterName").string.replace_with(countString)
+page.find(id="counterName").find('p').string.replace_with(countString)
 
 #write out the first data
 testV1spec = "var testV1Spec =" + chart.to_json() + "; vegaEmbed('#vis1', testV1Spec); "

380232b3d5c5a454abe6a3bd31adbd6bb01a60db
CoreyBurger <corey.burger@gmail.com>
2019-08-01 09:54:38 -0700

    major changes to visulization, start of monthly wording

--- visualizeCounter.py
+++ visualizeCounter.py
@@ -6,11 +6,15 @@ import datetime
 import bs4
 import os
 import csv
+import statistics
 
 #define constants
 workingDir = os.getcwd()
 yesterday = datetime.date.today() - datetime.timedelta(1)
 yesterday = yesterday.strftime('%Y-%m-%d')
+yesterdayDay = (datetime.date.today() - datetime.timedelta(1)).day
+yesterdayMonth = (datetime.date.today() - datetime.timedelta(1)).month
+yesterdayMonthName =  (datetime.date.today() - datetime.timedelta(1)).strftime("%B")
 
 #load data
 countFile = "counts-100117730.csv"
@@ -53,7 +57,22 @@ monthlyCount['Month'] = monthlyCount.index
 monthlyCount = monthlyCount.loc[monthlyCount['Month'] >= '2015-01-31']
 
 #Pull out the current month
-currentMonth = pandas.DataFrame(dailyCount.loc[dailyCount.index.month==datetime.datetime.now().month])
+currentMonth = pandas.DataFrame(dailyCount.loc[dailyCount.index.month==yesterdayMonth])
+
+#check if we are ahead of behind of the average monthly cumulative sum
+
+yesterdayMonthCumSumMean=statistics.mean(dailyCount.loc[(dailyCount.index.month==yesterdayMonth) & (dailyCount.index.day==yesterdayDay)].MonthlyCumSum)
+yesterdayMonthlyCumSum=pandas.DataFrame(dailyCount.loc[dailyCount.index==yesterday]).MonthlyCumSum[0]
+yesterdayMonChange=(yesterdayMonthlyCumSum-yesterdayMonthCumSumMean)/yesterdayMonthCumSumMean
+
+if yesterdayMonChange<-0.05:
+    monthlyCountString='less busy than'
+elif yesterdayMonChange>0.05:
+    monthlyCountString='busier than'
+else:
+    monthlyCountString='about as busy same as'
+
+MonthlyCountString=  yesterdayMonthName +  " is " + monthlyCountString + " average"
 
 #Create daily heatmap
 heatmap = altair.Chart(dailyCount).mark_rect().encode(
@@ -63,30 +82,33 @@ heatmap = altair.Chart(dailyCount).mark_rect().encode(
 ).properties(width=200,height=300)
 
 #Create monthly cumulative line chart
-monthlyLine = altair.Chart(currentMonth).mark_line().encode(
+monthlyLine = altair.Chart(currentMonth).mark_line(stroke='#f304d3').encode(
     altair.X('date(Day):O'),
     altair.Y('MonthlyCumSum', axis=altair.Axis(title='Monthly Cumulative Sum')),
     altair.Color('Day:O', timeUnit='year')  
-).properties(width=200,height=300)
+).transform_filter(
+    altair.FieldEqualPredicate(timeUnit='year',field='Day',equal=datetime.date.today().year)
+).properties(width=200,height=200)
 
-monthlyLineAvg = altair.Chart(currentMonth).mark_line(color='grey').encode(
+monthlyLineAvg = altair.Chart(currentMonth).mark_line(color='grey',strokeDash=[8,8]).encode(
     altair.X('date(Day):O'),
     altair.Y('mean(MonthlyCumSum)'),
-).properties(width=200,height=300)
+).properties(width=200,height=200)
 
 monthlyLineBand = altair.Chart(currentMonth).mark_errorband(extent='stdev',color='grey').encode(
     altair.X('date(Day):O'),
     altair.Y('MonthlyCumSum'),
-).properties(width=200,height=300)
+).properties(width=200,height=200)
 
 MonthlyChart = monthlyLineBand + monthlyLine + monthlyLineAvg
 
 #Create yearlycumulative line chart
+#TODO fix, not working correctly
 yearlyLine = altair.Chart(dailyCount).mark_line().encode(
     altair.X('Day:O', timeUnit='month'),
     altair.Y('YearlyCumSum', axis=altair.Axis(title='Yearly Cumulative Sum')),
     altair.Color('Day:O', timeUnit='year')  
-).properties(width=200,height=300)
+).properties(width=200,height=200)
 
 chart = altair.hconcat(heatmap,MonthlyChart,yearlyLine)
 
